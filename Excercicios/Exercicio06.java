6f5562c87d823218d652298ae9081f62ecdb3f11
brunofreitas882 <115594724+brunofreitas882@users.noreply.github.com>
2023-07-27 21:50:36 -0300

    Add files via upload

---
+++ Excercicios/Exercicio06.java
@@ -0,0 +1,33 @@
+package Excercicios;
+
+public class Exercicio06 {
+	public static void main(String[] args) {
+		double a = 3.0;
+		double b = 4.0;
+		double c = 5.2;
+
+		
+
+		
+	        System.out.printf("TRIANGULO: %.3f%n", (a * c) / 2);
+	        System.out.printf("CIRCULO: %.3f%n", 3.14159 * (c * c));
+	        System.out.printf("TRAPEZIO: %.3f%n", ((a + b) * c) / 2);
+	        System.out.printf("QUADRADO: %.3f%n", b * b);
+	        System.out.printf("RETANGULO: %.3f%n", a * b);
+	}
+
+}
+
+
+//double triangleArea = (a * c) / 2;
+//double circleArea = 3.14159 * Math.pow(c, 2);
+//double trapezoidArea = ((a + b) * c) / 2;
+//double squareArea = Math.pow(b, 2);
+//double rectangleArea = a * b;
+
+// Exibição dos resultados
+//System.out.printf("TRIANGULO: %.3f%n", triangleArea);
+//System.out.printf("CIRCULO: %.3f%n", circleArea);
+//System.out.printf("TRAPEZIO: %.3f%n", trapezoidArea);
+//System.out.printf("QUADRADO: %.3f%n", squareArea);
+//System.out.printf("RETANGULO: %.3f%n", rectangleArea);
